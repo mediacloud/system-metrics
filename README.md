92172de30a747fe78595a7eae67c00a64f135a96
Paige Gulley <pgulley@ludwig.local>
2024-06-27 22:33:28 -0400

    deployment script

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 
 Is this an excersize over-engineering? 
 Yes absolutely. 
+
+Running deploy.py creates the dployment within prefect cloud.
--- main.py
+++ main.py
@@ -4,21 +4,23 @@ from prefect import flow, get_run_logger
 from prefect.runtime import flow_run
 from sous_chef import RunPipeline, recipe_loader
 
-
+@flow()
 def RunMetrics():
-	mixins_file = open("queries.yaml").read()
+	mixins_file = open("queries.yaml", "r")
 	recipe_file = open("QueryRecipe.yaml").read()
 
 	mixins = recipe_loader.load_mixins(mixins_file)
-
+	
 	run_data = {}
 	for template_params in mixins:
 		json_conf = recipe_loader.t_yaml_to_conf(recipe_file, **template_params)
-		print(json_conf)
-		run_data_ = RunPipeline(json_conf)
-		print(run_data_)
+		json_conf["name"] = template_params["NAME"]
 
-
+		results = RunPipeline(json_conf)
+		
+		run_data[template_params["NAME"]] = list(results.values())
+	
+	print(run_data)
 
 if __name__ == "__main__":
 	RunMetrics()
