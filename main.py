415195d46b9de0fbf538b7e30c041a2957da3d4e
Paige Gulley <pgulley@campus-vpn-172-26-69-106.vpn.umass.edu>
2024-07-02 16:56:52 -0400

    reporting metrics to statsd, and added daf queries, and removed unused deployment infra

--- main.py
+++ main.py
@@ -6,13 +6,19 @@ from prefect.deployments import Deployment
 from prefect.infrastructure import Process
 
 from sous_chef import RunPipeline, recipe_loader
+import statsd
+
+statsd_url = "statsd://stats.tarbell.mediacloud.org"
+stats_directory = "metrics/query-benchmark/"
 
 @flow()
-def RunMetrics():
+def RunMetrics(test=False):
 	mixins_file = open("queries.yaml", "r")
 	recipe_file = open("QueryRecipe.yaml").read()
 
 	mixins = recipe_loader.load_mixins(mixins_file)
+	if test:
+		mixins = [mixins[0]]
 	
 	run_data = {}
 	for template_params in mixins:
@@ -23,7 +29,15 @@ def RunMetrics():
 		
 		run_data[template_params["NAME"]] = list(results.values())
 	
-	print(run_data)
+	statsd_client = statsd.StatsdClient(
+		statsd_url, None, stats_directory)
+
+	for name, value in run_data.items():
+		print(f"{name}:{value}")
+		statsd_client.timing(name, value)
+
+	###Then I think we want to report a timing metric to statsd for each of these runs? is that right?
+
 
 	
 
@@ -32,21 +46,9 @@ if __name__ == "__main__":
 	#Continually annoyed by building images in prefect. 
 	#Not sure how to get the requirements into this image here in a way that feels nice to include as code. 
 	parser = argparse.ArgumentParser()
-	parser.add_argument("-d", "--deploy", action='store_true')
-
+	parser.add_argument("-t", "--test", action='store_true')
 	args = parser.parse_args()
+	RunMetrics(args.test)
 
-	if args.deploy:
-		Deployment.build_from_flow(
-	        flow=RunMetrics, 
-       		name="daily-metrics",
-        	work_pool_name="Guerin",
-        	infrastructure=Process(
-		        env={"PIP_REQUIREMENTS": "requirements.txt"}
-		    )
-    	).apply()
-
-	else:
-		RunMetrics()
 
 
